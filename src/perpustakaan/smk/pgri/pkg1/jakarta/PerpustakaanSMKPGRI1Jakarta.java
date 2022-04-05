64faf1d40edd54771f56fe292db7734c935f537c
Atthoriq <thoriqaziz.muhammad@gmail.com>
2022-04-05 19:20:28 +0700

    Upload1

---
+++ src/perpustakaan/smk/pgri/pkg1/jakarta/PerpustakaanSMKPGRI1Jakarta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perpustakaan.smk.pgri.pkg1.jakarta;
+
+/**
+ *
+ * @author Atthoriq
+ */
+public class PerpustakaanSMKPGRI1Jakarta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
